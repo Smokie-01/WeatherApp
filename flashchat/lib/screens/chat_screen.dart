ddb18101d655827729aad547593d5050cfe08973
Smokie-01 <2002taufiqmulla@gmail.com>
2022-12-10 12:57:57 +0530

    t

--- flashchat/lib/screens/chat_screen.dart
+++ flashchat/lib/screens/chat_screen.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import '../constants.dart';
-import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatefulWidget {
   static String chatid = "chat_screen";
@@ -10,8 +10,10 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  late String messages;
+  final _fireStore = FirebaseFirestore.instance;
   final _auth = FirebaseAuth.instance;
-  late FirebaseUser LoggedInUser;
+  late User LoggedInUser;
 
   @override
   void initState() {
@@ -21,13 +23,21 @@ class _ChatScreenState extends State<ChatScreen> {
 
   void getCurrentUser() async {
     try {
-      final user = await _auth.currentUser();
+      final user = await _auth.currentUser;
       if (user != null) {
         LoggedInUser = user;
         print(LoggedInUser.email);
       }
     } catch (e) {
       print(e);
+    }
+  }
+
+  void messageStream() async {
+    await for (var snapshot in _fireStore.collection('messages').snapshots()) {
+      for (var message in snapshot.docs) {
+        print(message.data());
+      }
     }
   }
 
@@ -40,7 +50,7 @@ class _ChatScreenState extends State<ChatScreen> {
           IconButton(
               icon: Icon(Icons.close),
               onPressed: () {
-                //Implement logout functionality
+                messageStream();
               }),
         ],
         title: Text('⚡️Chat'),
@@ -51,6 +61,32 @@ class _ChatScreenState extends State<ChatScreen> {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
+            StreamBuilder(
+              stream: _fireStore.collection('messages').snapshots(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return Center(
+                    child: CircularProgressIndicator(
+                      backgroundColor: Colors.lightBlue,
+                    ),
+                  );
+                }
+                final messages = snapshot.data;
+                List<Text> messagesWidgets = [];
+                for (var message in messages) {
+                  final messageText = messages.data['text'];
+                  final messageSender = messages.data['sender'];
+
+                  final messageWidget =
+                      Text('$messageText from $messageSender');
+                }
+                return Column(
+                  children: [
+                    messagesWidgets,
+                  ],
+                );
+              },
+            ), //stream : means from where the data come from
             Container(
               decoration:
                   kMessageContainerDecoration, //kMessageContainerDecoration,
@@ -60,14 +96,15 @@ class _ChatScreenState extends State<ChatScreen> {
                   Expanded(
                     child: TextField(
                       onChanged: (value) {
-                        //Do something with the user input.
+                        messages = value;
                       },
                       decoration: kMessageTextFieldDecoration,
                     ),
                   ),
                   ElevatedButton(
                     onPressed: () {
-                      //Implement send functionality.
+                      _fireStore.collection('messages').add(
+                          {"text": messages, "sender": LoggedInUser.email});
                     },
                     child: Text(
                       'Send',
